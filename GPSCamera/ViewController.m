63c5515008c0a36a4e99d41e5ce6023e5630211f
rvill <data-03@rvills-Mac-mini.local>
2015-05-25 17:22:43 -0700

    updated distance filter values

--- GPSCamera/ViewController.m
+++ GPSCamera/ViewController.m
@@ -14,11 +14,11 @@
 @end
 
 //first two used in setting up the CLLocationManager
-#define kDistanceFilter 25
+#define kDistanceFilter 1 //25m, will not return a new position unless its 1m away, 1m between each point
 #define kHeadingFilter 0
 //these two used when refining the results further
-#define kAccuracyFilter 50
-#define kTimeFilter 0.2
+#define kAccuracyFilter 1 //50m
+#define kTimeFilter 0.2 //0.2 seconds
 
 @implementation ViewController
 
@@ -28,7 +28,7 @@
     self.locationManager = [[CLLocationManager alloc]init];
     self.locationManager.delegate = self;
     //add
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;//kCLLocationAccuracyBestForNavigation;
     self.locationManager.distanceFilter = kDistanceFilter;
     self.locationManager.headingFilter = kHeadingFilter;
     
@@ -45,10 +45,7 @@
     [[LocationHandler getSharedInstance]startUpdating];
     // Do any additional setup after loading the view, typically from a nib.
     
-  
-    
-    
-   
+
     
 }
 
@@ -57,22 +54,31 @@
     // Dispose of any resources that can be recreated.
 }
 
+//if the locations have passed the criteria, they're then set here:
 -(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
     
+    //timestamp of cllocation object and check it against the current time to get its age and decide if it's recent enough to be useful.
+    
+    //fix degrees of inaccurate signals, turn negative float to positive values.
     NSTimeInterval timeSince = fabs([newLocation.timestamp timeIntervalSinceNow]);
     if ((newLocation.horizontalAccuracy < kAccuracyFilter) && (timeSince < kTimeFilter))
     {
-        //[self.delegate waypoint:newLocation];
+        
+       // [self.delegate waypoint:newLocation];
     } else {
         NSLog(@"Waypoint discarded. Accuracy : %f, Age : %f", newLocation.horizontalAccuracy, timeSince);
     }
     NSDate *eventsDate = newLocation.timestamp;
     NSTimeInterval eventinterval = [eventsDate timeIntervalSinceNow];
     
-   
+    //minimum of 5 seconds between points
     if (timeSince > 5)
     {
         //use the waypoint ..
+        [latitudeLabel setText:[NSString stringWithFormat:
+                                @"Latitude: %f",newLocation.coordinate.latitude]];
+        [longitudeLabel setText:[NSString stringWithFormat:
+                                 @"Longitude: %f",newLocation.coordinate.longitude]];
     } else {
         NSLog(@"Not 5s since previous so not adding");
     }
@@ -87,18 +93,29 @@
     NSLog(@"%",[locations lastObject]);
 }*/
 
--(void)didUpdateToLocation:(CLLocation *)newLocation
+/*-(void)didUpdateToLocation:(CLLocation *)newLocation
               fromLocation:(CLLocation *)oldLocation{
     [latitudeLabel setText:[NSString stringWithFormat:
                             @"Latitude: %f",newLocation.coordinate.latitude]];
     [longitudeLabel setText:[NSString stringWithFormat:
                              @"Longitude: %f",newLocation.coordinate.longitude]];
     NSLog(@"lat = %f, lon = %f",newLocation.coordinate.latitude, newLocation.coordinate.longitude);
-    //NSLog(@"lon= %f", newLocation.coordinate.longitude);
+   
     
     
+}*/
+
+//to call to start and stop getting GPS from the receiver
+- (void) startUpdates
+{
+    [self.locationManager startUpdatingLocation];
+    [self.locationManager startUpdatingHeading];
 }
 
-
+- (void) stopUpdates
+{
+    [self.locationManager stopUpdatingLocation];
+    [self.locationManager stopUpdatingHeading];
+}
 
 @end
